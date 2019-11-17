91ad31788986a38e350fbe1348c5e75b94301f1b
Daniel Woodhead <Daniel.Woodhead@emishealth.com>
2019-11-17 15:00:35 +0000

    Added call API to MVC client

--- src/services/identity/src/Identity.API/Config.cs
+++ src/services/identity/src/Identity.API/Config.cs
@@ -44,10 +44,11 @@ namespace Identity.API
                     AllowedScopes = new List<string>
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile
-                    }
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    },
+                    AllowOfflineAccess = true
                 }
             };
-
     }
 }
