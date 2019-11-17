b3474a176f064426462a4b878744c0553c4f6b56
Daniel Woodhead <Daniel.Woodhead@emishealth.com>
2019-11-17 14:02:40 +0000

    Initial identity service commit

---
+++ src/services/identity/src/Identity.API/Config.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace Identity.API
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+
+    }
+}
