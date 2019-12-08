3b029d39fab73fb92859e2310b8500d59829f3e0
Daniel Woodhead <Daniel.Woodhead@emishealth.com>
2019-12-08 17:44:50 +0000

    Initial build/deployment files

---
+++ src/services/identity/build/build.ps1
@@ -0,0 +1,25 @@
+﻿Param(
+    [parameter(Mandatory=$true)][string]$subscription,
+    [parameter(Mandatory=$true)][string]$resourceGroupName,
+    [parameter(Mandatory=$false)][string]$acrName,
+    [parameter(Mandatory=$false)][string]$gitUser="danielwoodhead",
+    [parameter(Mandatory=$false)][string]$repoName="app",
+    [parameter(Mandatory=$false)][string]$gitBranch="master"
+)
+
+$gitContext = "https://github.com/$gitUser/$repoName.git#$gitBranch"
+
+$services = @(
+    @{ Image="app/identity.api.$gitBranch`:v1"; Context="src/services/identity/src"; File="Identity.API/Dockerfile" }
+)
+
+Write-Host "Setting subscription..." -ForegroundColor Yellow
+az account set --subscription $subscription
+
+$services |% {
+    $image = $_.Image
+    $context = $_.Context
+    $file = $_.File
+    Write-Host "Setting ACR build $image $context/$file"
+    az acr build --registry $acrName $gitContext`:$context --image ${image} --file ${file} --resource-group $resourceGroupName
+}
---
+++ src/services/identity/deploy/k8s/deploy.ps1
@@ -0,0 +1,4 @@
+﻿# auth helm with ACR
+az acr helm repo add
+
+helm install identity-api --debug --dry-run ./identity-api
