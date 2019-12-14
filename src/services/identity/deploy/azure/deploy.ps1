193c31b958326cb2e04f7b17a54ad9a9290d6e6e
danielwoodhead <danny2311@hotmail.com>
2019-12-14 19:09:55 +0000

    ARM templates to deploy shared app service plan and identity app service (#4)

---
+++ src/services/identity/deploy/azure/deploy.ps1
@@ -0,0 +1,2 @@
+az account set --subscription bc22730d-89ef-4562-9a78-dfb790976b9a
+az group deployment create --resource-group DansMicroservices --template-file azuredeploy.json --parameters azuredeploy.parameters.json
