1c2f2f7caa2c041c9aff73b641ec0e90bfd31eb3
Daniel Woodhead <Daniel.Woodhead@emishealth.com>
2019-12-15 13:41:27 +0000

    Deployment to storage account

--- src/web/src/components/publicPage.jsx
+++ src/web/src/components/publicPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const PublicPage = () => {
     return (
         <div>
             Public page
+            <br />
+            <Link to={`/private`}>Private</Link>
         </div>
     );
 };
