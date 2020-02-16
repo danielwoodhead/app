1db17c022e4444b0fd1f90bcce4728eb34e319fa
danielwoodhead <danny2311@hotmail.com>
2020-02-16 21:45:12 +0000

    Changes to deploy SQL version and enable batch and transactions (#8)

--- src/services/fhir/deploy/deploy.ps1
+++ src/services/fhir/deploy/deploy.ps1
@@ -1,3 +1,3 @@
 ﻿az account set --subscription bc22730d-89ef-4562-9a78-dfb790976b9a
 az group create --location uksouth --name DansFhir
-az group deployment create --resource-group DansFhir --template-file default-azuredeploy.json --parameters default-azuredeploy.parameters.json
+az group deployment create --resource-group DansFhir --template-file default-azuredeploy-sql.json --parameters default-azuredeploy-sql.parameters.json --parameters sqlAdminPassword={password}
