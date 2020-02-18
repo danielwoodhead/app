312f44fc9bdd81a4c8b6322d290c39e47396d8dd
Daniel Woodhead <Daniel.Woodhead@emishealth.com>
2020-02-18 10:22:25 +0000

    naming

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_container_registry" "cr" {
 }
 
 resource "azurerm_application_insights" "ai" {
-  name                = "${var.prefix}insights"
+  name                = "${var.prefix}-insights"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
