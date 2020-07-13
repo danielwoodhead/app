f731288ed291a76923075ef5692a2b377768ce4c
Daniel Woodhead <danny2311@hotmail.com>
2020-07-13 21:56:01 +0100

    Replace identity server

--- src/services/identity/deploy/obsolete/k8s/helm/push.ps1
+++
@@ -1,2 +0,0 @@
-﻿helm package .\identity-api
-az acr helm push identity-api-0.1.0.tgz
--- src/services/identity/src/MyHealth.Identity.Api/Areas/Admin/Pages/Index.cshtml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace MyHealth.Identity.Api.Areas.Admin.Pages
-{
-    public class IndexModel : PageModel
-    {
-    }
-}
